bb9025771682941bb5784c7ac67ab93a010a5406
Anant Jain <anant90@gmail.com>
2018-08-29 21:01:53 -0700

    Minor fixes

--- imports/ui/components/employees_detail.js
+++ imports/ui/components/employees_detail.js
@@ -1,21 +1,19 @@
 import React from 'react';
- 
+
 const EmployeeDetail = ( {employee} ) => {  //using ES6 syntax instead of props
-  
   const {name, email, phone,avatar} = employee;   //destructuring syntax
- 
   return (
-      <div className="thumbnail">
-          <img src={avatar} />
-          <div className="caption">
-              <h3>{name}</h3>
-              <ul className="list-group">
-                  <li className="list-group-item"> Email: {email}</li>
-                  <li className="list-group-item"> Phone: {phone}</li>
-              </ul>
-          </div>
+    <div className="thumbnail">
+      <img src={avatar} />
+      <div className="caption">
+        <h3>{name}</h3>
+        <ul className="list-group">
+          <li className="list-group-item"> Email: {email}</li>
+          <li className="list-group-item"> Phone: {phone}</li>
+        </ul>
       </div>
+    </div>
   );
 };
- 
+
 export default EmployeeDetail;
--- imports/ui/components/employees_list.js
+++ imports/ui/components/employees_list.js
@@ -3,44 +3,43 @@ import React, {Component} from 'react';
 import { Employees } from '../../api/employees';
 import { withTracker } from 'meteor/react-meteor-data';
 import EmployeeDetail from './employees_detail';
- 
+
 const PAGE_VALUE = 20;
- 
+
 class EmployeeList extends Component{
   constructor(props){
     super(props);
     this.page = 1
   }
- 
+
   onClickPage(){
     this.page+=1;
     Meteor.subscribe('employees', PAGE_VALUE * this.page);
   }
- 
-    render(){
+
+  render(){
     //this.props.employees => an array of employee objects
-  //console.log(this.props.employees);
-          return(
-                <div>
-                      <h2 className="text text-primary text-center"> My Employees </h2>
-                      <div className="employee-list">
-                            {this.props.employees.map((employee) =>
-                                  <EmployeeDetail key={employee._id} employee={employee} />
-                            )}
-                      </div>
-                      <button onClick= { this.onClickPage.bind(this) }
-                      className="btn btn-primary">
-                      Load more
-                </button>
-                </div>
-          );
-    };
+    //console.log(this.props.employees);
+    return(
+      <div>
+        <h2 className="text text-primary text-center"> My Employees </h2>
+        <div className="employee-list">
+          {this.props.employees.map((employee) =>
+            <EmployeeDetail key={employee._id} employee={employee} />
+          )}
+        </div>
+        <button onClick= { this.onClickPage.bind(this) } className="btn btn-primary">
+          Load more
+        </button>
+      </div>
+    );
+  };
 };
- 
+
 export default withTracker(()=>{
   //set up the subscription
   Meteor.subscribe('employees', PAGE_VALUE);
   //return an object. Whatever is returned will
   //be sent to EmployeeList as props
   return {employees: Employees.find({}).fetch()};
-}) (EmployeeList);
+})(EmployeeList);
